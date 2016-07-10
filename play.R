777a22c6d90ca849cbf817d775e76d64b16d9c77
jakecoltman <jakecoltman@sky.com>
2016-07-10 13:25:14 +0100

    Matrices and vector functions

--- play.R
+++ play.R
@@ -21,3 +21,29 @@ rm(list = ls()) # Clear everything
 ### Matrix
 
 x = matrix(data = c(1,2,3,4), nrow = 2, ncol = 2)
+x_t = matrix(data = c(1,2,3,4), nrow = 2, ncol = 2, byrow = TRUE)
+x_t
+x
+
+### Element wise calcualtions
+
+sqrt(x)
+x ^ 2
+
+### Randomness
+
+x = rnorm(100)
+y = rnorm(100)
+cor(x, y)
+
+z = x + rnorm(100, mean = 10, sd= 1)
+cor(z, x)
+
+set.seed(1303)
+rnorm(50)
+
+### Moments
+
+set.seed(3)
+y = rnorm(100)
+y
